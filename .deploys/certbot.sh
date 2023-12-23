e6b19009fcd90a5c2f0faea28979e1b73db0936f
Hung Huynh <hung.huynh@viviix.com>
2023-12-23 17:43:18 +0700

    build: update dist path in certbot

--- .deploys/certbot.sh
+++ .deploys/certbot.sh
@@ -1,1 +1,1 @@
-sudo certbot certonly --webroot --agree-tos --email camhungh@gmail.com -d nikehotay.com -w ./html/ 
+sudo certbot certonly --webroot --agree-tos --email camhungh@gmail.com -d nikehotay.com -w ../dist/ 
